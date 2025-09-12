1a2573d4be6e89d9ec1fb2b98a0d46d4d2bc2e9e
Paolo Fischer <paolo123@OPENMIND7.MIT.EDU>
2025-09-12 16:12:06 -0400

    implemented MFM loss manager

---
+++ loss/default_source_only.py
@@ -0,0 +1,49 @@
+import torch
+
+class LossManager:
+    def __init__(self):
+
+    def loss(self, encoder, generator, predictor, batch, device):
+        losses = {}
+        loss = 0
+
+        # Handle both tensor and dictionary samples for source and target
+        if isinstance(batch['source_samples'], torch.Tensor):
+            source_samples = batch['source_samples'].to(device)
+            target_samples = batch['target_samples'].to(device)
+            
+            source_latent = encoder(source_samples)
+            target_latent = None
+                
+            recon_loss = generator.loss(
+                source_samples.view(-1, *source_samples.shape[2:]), 
+                target_samples.view(-1, *target_samples.shape[2:]),
+                source_latent, 
+                target_latent
+            )
+        else:
+            # For dictionary samples (like PubMed dataset), move tensors to device
+            source_samples = {}
+            target_samples = {}
+            
+            for key, value in batch['source_samples'].items():
+                if isinstance(value, torch.Tensor):
+                    source_samples[key] = value.to(device)
+                else:
+                    source_samples[key] = value
+                    
+            for key, value in batch['target_samples'].items():
+                if isinstance(value, torch.Tensor):
+                    target_samples[key] = value.to(device)
+                else:
+                    target_samples[key] = value
+
+            # Encode samples to latent space
+            source_latent = encoder(source_samples)
+            target_latent = None
+            
+            recon_loss = generator.loss(source_samples, target_samples, source_latent, target_latent)
+
+        loss += recon_loss
+        losses['reconstruction_loss'] = recon_loss
+        return loss, losses
